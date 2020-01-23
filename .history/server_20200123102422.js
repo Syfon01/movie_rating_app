977a0a2b3ed4ffe24dec80f63a16cbd6eaed8dd7
Sifon Isaac <syfonisaac@gmail.com>
2020-01-23 10:41:32 +0100

    authentication added

--- .history/server_20200123102422.js
+++ .history/server_20200123102422.js
@@ -5,19 +5,12 @@ const cors = require('cors');
 const morgan = require('morgan');
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
-const passport = require('passport');
-const passportJWT = require("passport-jwt");
-const ExtractJwt = passportJWT.ExtractJwt;
-const JwtStrategy = passportJWT.Strategy;
-const jwtOptions = {};
-jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
-jwtOptions.secretOrKey = "movieratingapplicationsecretkey";
+const passport = require
 const app = express();
 const router = express.Router();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(passport.initialize());
 //connect to mongodb
 mongoose.connect('mongodb://localhost/movie_rating_app', function () {
     console.log('Connection has been made');
